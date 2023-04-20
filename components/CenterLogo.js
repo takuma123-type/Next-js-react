feb242c27feb016c38791a0c9d1d09d85f25a869
takuma123-type <seishirou1270@gmail.com>
2023-04-21 02:08:42 +0900

    Footer部分とLogo部分のコンポーネント化

---
+++ components/CenterLogo.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export function CenterLogo() {
+  return (
+    <div className={styles.center}>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+    </div>
+  )
+}
