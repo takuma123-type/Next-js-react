d05e867c4cfb454e8ba88abb98b368cca0a4a634
takuma123-type <seishirou1270@gmail.com>
2023-04-21 15:21:38 +0900

    ✨ Main部分のコンポーネント化

---
+++ components/MainTop.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export function MainTop() {
+  return (
+    <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>pages/index.js</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+      </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Footer } from '@/components/Footer'
 import { CenterLogo } from '@/components/CenterLogo'
+import { MainTop } from '@/components/MainTop'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,33 +17,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <MainTop />
       <CenterLogo />
       <Footer />
-      </main>
     </>
   )
 }
